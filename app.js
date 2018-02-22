bd10e8484f3a160183627c793b88db52e23131fb
tanner heaps <tanner.heaps@gmail.com>
2018-02-22 12:56:25 -0600

    made some of the buttons work

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     $('.headerbutton').on('click', function() {
-        $('.lanes').append('<div class="lane"><div class="lanebuttons"><button class="move purple"><i class="fa fa-angle-left"></i></button><button class="lanebutton removebutton">Remove Lane</button><button class="lanebutton addcard">Add A Card</button><button class="move purple"><i class="fa fa-angle-right"></i></button></div></div>')
+        $('.lanes').append('<div class="lane removable-container"><div class="lanebuttons"><button class="move purple"><i class="fa fa-angle-left"></i></button><button class="lanebutton removebutton">Remove Lane</button><button class="lanebutton addcard">Add A Card</button><button class="move purple"><i class="fa fa-angle-right"></i></button></div></div>')
 
     });
 
-    $('.lanes').on('click','removebutton', function(){
-    	 $(this).closest('.card').remove();
+    $('.lanes').on('click','.removebutton', function(){
+    	 $(this).closest('.removable-container').remove();
     });
     $('.lanes').on('click','.addcard', function(){
-    	$('.lane').append('<div class="card"><div class="cardbuttons"><button class="move blue"><i class="fa fa-angle-left"></i></button><button class="cardbutton">Add Task</button><button class="cardbutton removebutton">Remove Card</button><button class="move blue"><i class="fa fa-angle-right"></i></button></div></div>')
+
+    	$(this).closest('.lane').append('<div class="card removable-container"><div class="cardbuttons"><button class="move blue"><i class="fa fa-angle-left"></i></button><button class="cardbutton">Add Task</button><button class="cardbutton removebutton">Remove Card</button><button class="move blue"><i class="fa fa-angle-right"></i></button></div></div>')
     });
 
 });
